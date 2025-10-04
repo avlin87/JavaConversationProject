742c0abd2ff05b49136ebc8a2394fe132a219c7c
Pablo Aleman <pabart.aleman@gmail.com>
2025-10-03 22:06:56 -0600

    Adds --user-data-dir flag directly to yml file

--- src/test/java/com/testbase/TestBase.java
+++ src/test/java/com/testbase/TestBase.java
@@ -2,24 +2,17 @@ package com.testbase;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.UUID;
 
 public class TestBase {
-    private static final Logger log = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeAll
     static void setup() {
         Configuration.browser = "chrome";
         Configuration.headless = true;
         Configuration.browserSize = "1920x1080";
-
-        String randomUUIDvalue = UUID.randomUUID().toString();
-
-        log.info("randomUUIDvalue: {}", randomUUIDvalue);
 
         // Add Chrome options
         Configuration.browserCapabilities.setCapability(
