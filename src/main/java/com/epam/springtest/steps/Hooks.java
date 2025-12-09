ecfbcae81d20cfa2267e1320382cac10a8ec0fd0
Liadov_A_V <33938644+avlin87@users.noreply.github.com>
2025-12-09 14:08:28 -0700

    split test case (#7)

--- src/main/java/com/epam/springtest/steps/Hooks.java
+++ src/main/java/com/epam/springtest/steps/Hooks.java
@@ -2,24 +2,29 @@ package com.epam.springtest.steps;
 
 import com.codeborne.selenide.WebDriverRunner;
 import com.epam.springtest.config.BrowserFactory;
+import com.epam.springtest.util.DriverConfig;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 @Slf4j
+@RequiredArgsConstructor
 public class Hooks {
 
+    private final DriverConfig driverConfig;
+
     @Before
-    public void logScenarioStart(Scenario scenario){
+    public void logScenarioStart(Scenario scenario) {
         log.info("\n** Starting scenario: {} **", scenario.getName());
     }
 
     @Before
     public void setUp() {
-        BrowserFactory.startBrowser(true);
+        BrowserFactory.startBrowser(driverConfig.isHeadless());
         WebDriverRunner.setWebDriver(BrowserFactory.getDriver());
     }
 
